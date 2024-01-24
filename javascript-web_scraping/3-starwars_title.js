2013df447ecdd4d62da0d34e7b6e658ef9c6e6ac
Mateo Zouko <mateozouko1@gmail.com>
2024-01-24 22:55:43 +0000

    task 3

---
+++ javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const request = require('request');
+
+const movieID = process.argv[2];
+const urlAPI = 'https://swapi-api.hbtn.io/api/films/:id';
+
+request.get(urlAPI, (error, response, body) => {
+  if (error) {
+    console.log('Error:', error);
+  }
+  const jsonRsp = JSON.parse(body);
+
+  if (jsonRsp.title) {
+    console.log(jsonRsp.title);
+  }
+});
