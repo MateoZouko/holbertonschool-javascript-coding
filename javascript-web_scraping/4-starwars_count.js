42bfb945d459fecc4b1c7bfc1c34154fb2f572ca
Mateo Zouko <mateozouko1@gmail.com>
2024-01-25 13:17:14 +0000

    task 4

---
+++ javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+const request = require('request');
+const urlAPI = 'https://swapi-api.hbtn.io/api/films/';
+
+request.get(urlAPI, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    const responseData = JSON.parse(body).results;
+    const filteredResults = responseData.reduce((acc, movie) => {
+      for (const character of movie.characters) {
+        if (character.includes('18')) {
+          acc++;
+          break;
+        }
+      }
+      return acc;
+    }, 0);
+    console.log(filteredResults);
+  }
+});
